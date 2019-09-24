a2dd9f35ba4782bdce3dc9ff045da280a7d12067
iulianmail007 <38085647+iulianmail007@users.noreply.github.com>
2019-09-24 08:45:52 +0200

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,22 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+#image=iulianmail007/myrepo_u_do:altest
+
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run mymicrosrv --image=iulianmail007/myrepo_u_do:altest --port=5000 --replicas=2 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pod 
+
+kubectl get pod -o wide
+
 
 # Step 4:
 # Forward the container port to a host
-
+#other terminal:
+#kubectl port-forward mymicrosrv-59f5f95967-tsspw 5010:5000
+kubectl logs mymicrosrv-59f5f95967-tsspw
